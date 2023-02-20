397ebcaaf0c8fc00c11e933cfc46a3a4d53f103a
Gonza <gonzagramaglia@gmail.com>
2023-02-19 22:39:42 -0300

    I keep moving forward with the project

--- js-fcc/blackjack-game-fcc/index.js
+++ js-fcc/blackjack-game-fcc/index.js
@@ -4,18 +4,22 @@ let secondCard = 7
 let sum = firstCard + secondCard + 3 + 4 + 1
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
 console.log(sum)
 
 if (sum < 21){
-    console.log("Do you want to draw a new card? :)")
+    message = "Do you want to draw a new card? :)"
 }else if (sum === 21){
-    console.log("Wohoo! You've got Blackjack! :D")
+    message = "Wohoo! You've got Blackjack! :D"
     hasBlackJack = true
 }else {
-    console.log("You're out of the game! :(")
+    message = "You're out of the game! :("
     isAlive = false
 }
 
-console.log(hasBlackJack)
-console.log(isAlive)
+function startGame(){
+    console.log("Clicked")
+}
+
+console.log(message)
