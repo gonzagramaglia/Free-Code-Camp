d4af3c18c3349fe48d1e16b4e0f33d6a3ba90535
Gonza <gonzagramaglia@gmail.com>
2023-02-20 23:37:22 -0300

    Blackjack game Update
    Added randomness on cards

--- js-fcc/blackjack-game-fcc/index.js
+++ js-fcc/blackjack-game-fcc/index.js
@@ -1,12 +1,11 @@
 
-let firstCard = 5
-let secondCard = 7
-let cards = [firstCard, secondCard]
+let firstCard = 0
+let secondCard = 0
+let cards = []
 console.log(cards)
-let card = 9
-let sum = firstCard + secondCard
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el") // # para especificar que es un id
@@ -14,6 +13,11 @@ let cardsEl = document.querySelector(".cards-el") // . para especificar que es u
 let deck = ""
 
 function startGame(){
+    firstCard = getRandomCard()
+    secondCard = getRandomCard()
+    isAlive = true
+    sum = firstCard + secondCard
+    cards = [firstCard, secondCard]
     renderGame()
 }
 
@@ -43,14 +47,25 @@ function displayGameInfo(){
 }
 
 function newCard(){
+    let card = getRandomCard()
     sum += card
     cards.push(card)
-    startGame()
+    renderGame()
+}
+
+function getRandomCard(){
+    let temporalCard = Math.floor(Math.random()*13)+1
+    if (temporalCard ===1){
+        return 11
+    } else if (temporalCard<=9){
+        return temporalCard
+    } else {
+        return 10
+    }
 }
 
 
 // let array = [1,1,2,1]
 // array.splice(2,1,1)
 // console.log(array)
-
 
