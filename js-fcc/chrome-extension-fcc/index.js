cb9982460106ea126954098d18c9fffd5ad00fff
Gonza <gonzagramaglia@gmail.com>
2023-02-24 00:48:07 -0300

    Chrome extension project update
    Refactored code with parameters and arguments

--- js-fcc/chrome-extension-fcc/index.js
+++ js-fcc/chrome-extension-fcc/index.js
@@ -2,31 +2,42 @@ let myLeads = [ ]
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector(".input-btn")
 const deleteBtn = document.querySelector(".delete-btn")
+const tabBtn = document.querySelector(".tab-btn")
 const ulEl = document.querySelector("#ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+let tabs = [ 
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 inputBtn.addEventListener('click', function(){ 
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
     inputEl.value = ""
+})
+
+tabBtn.addEventListener('click', function(){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("url",JSON.stringify(myLeads))
+    render(myLeads)
 })
 
 deleteBtn.addEventListener('click', function(){ 
     localStorage.clear()
     myLeads = [ ]
-    renderLeads()
+    render(myLeads)
 })
 
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
         //ulEl.innerHTML += "<li>"+ myLeads[i] +"</li>"
         // let li = document.createElement("li")
         // li.textContent+=myLeads[i]
@@ -34,8 +45,8 @@ function renderLeads() {
         // listItems += "<li><a href='https:\/\/"+myLeads[i]+"' target='_blank'>"+ myLeads[i] +"</a></li>"
         listItems += `
             <li>
-                <a target='_blank' href='https://www.${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='https://www.${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
